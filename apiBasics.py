da90f793098edaac8f4aa5ff07ec5b01c90234ca
Cameron Gropp <cgropp@ucsd.edu>
2017-12-29 22:39:32 -0800

    got name and twitter handle extraction working, yay

--- apiBasics.py
+++ apiBasics.py
@@ -4,7 +4,7 @@ import json
 print("Code is running")
 
 #Open URL to make API call
-tournamentJSON = urlopen('https://api.smash.gg/tournament/mega-smash-mondays-126')
+tournamentJSON = urlopen("https://api.smash.gg/tournament/falcon-punch-fridays-46")
 #Load json file into variable
 tournamentData = json.load(tournamentJSON)
 
@@ -12,9 +12,23 @@ tournamentData = json.load(tournamentJSON)
 tournamentID = tournamentData["entities"]["tournament"]["id"]
 
 
-print(tournamentID) #Test successful, tournament ID is correct
+#print(tournamentID) #Test successful, tournament ID is correct
 
 #Construct URL of stream queue api call and open
-streamQueueURL = "https://api.smash.gg/station_queue/" + str(tournamentID)
-print(streamQueueURL)
-streamQueueJSON = urlopen(streamQueueURL)
+streamURL = "https://api.smash.gg/station_queue/" + str(tournamentID)
+
+#print(streamURL) #Test successful, url is correct
+
+streamJSON = urlopen(streamURL)
+
+streamData = json.load(streamJSON)
+
+#Extract data about players on stream
+gamerTag0 = str(streamData["data"]["entities"]["player"][0]["gamerTag"])
+twitter0 = str(streamData["data"]["entities"]["player"][0]["twitterHandle"])
+
+gamerTag1 = str(streamData["data"]["entities"]["player"][1]["gamerTag"])
+twitter1 = str(streamData["data"]["entities"]["player"][1]["twitterHandle"])
+
+
+print("Currently on stream: " + gamerTag0 + " (Twitter handle: " + twitter0 + ") vs. " + gamerTag1 + " (Twitter handle: " + twitter1 + ")")
